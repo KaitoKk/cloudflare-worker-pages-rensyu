edd82070a3ca19151b410e84edf8d30089a98db7
KaitoKk <casval.graham@gmail.com>
2022-10-10 17:34:13 +0900

    uploaderの実装

--- upload-app/src/index.ts
+++ upload-app/src/index.ts
@@ -8,6 +8,7 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+/*
 export interface Env {
 	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
 	// MY_KV_NAMESPACE: KVNamespace;
@@ -28,3 +29,48 @@ export default {
 		return new Response("Hello World!");
 	},
 };
+*/
+
+export interface Env {
+	images: R2Bucket
+}
+
+import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+import { etag } from 'hono/etag'
+
+const app = new Hono<{ Bindings: Env }>()
+app.use('*', cors(), etag())
+
+app.post('/upload', async (context) => {
+	const formData = await context.req.formData()
+	const file = formData.get("image") as File
+
+	const ext = file.name.split('.').pop()
+	const id = Date.now().toString() + '.' + ext
+
+	await context.env.images.put(id, await file.arrayBuffer())
+
+	return context.json({ message: 'ok', id }, 200)
+})
+
+app.get('/:id', async (context) => {
+	const id = context.req.param('id')
+	const body = await context.env.images.get(id)
+
+	if (!body) return context.json({ error: 'not found' }, 404)
+
+	return context.body(body.body, 200)
+})
+
+app.delete('/:id', async (context) => {
+	const id = context.req.param('id')
+	const body = await context.env.images.get(id)
+
+	if (!body) return context.json({ error: 'not found' }, 404)
+
+	await context.env.images.delete(id)
+	return context.json({ message: 'ok' }, 200)
+})
+
+export default app
