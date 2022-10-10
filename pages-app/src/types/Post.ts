1a4900b139a13a58e8607526e82ee3fd515a2613
KaitoKk <casval.graham@gmail.com>
2022-10-11 00:34:02 +0900

    add type

---
+++ pages-app/src/types/Post.ts
@@ -0,0 +1,16 @@
+export type Post = {
+    datetime: number,
+    text: string,
+    lat?: string,
+    lon?: string,
+    location?: string,
+    images?: string[]
+}
+
+export type Param = {
+    text: string,
+    lat?: string,
+    lon?: string,
+    location?: string,
+    images?: string[]
+}
