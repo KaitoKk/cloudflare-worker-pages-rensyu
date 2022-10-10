7b4c33cc79a83cbea1f227240fc6a6c0179a6bbd
KaitoKk <casval.graham@gmail.com>
2022-10-11 00:06:11 +0900

    config修正

--- pages-app/vue.config.js
+++ pages-app/vue.config.js
@@ -1,15 +1,15 @@
 const bootstrapSassAbstractsImports = require('vue-cli-plugin-bootstrap-vue/sassAbstractsImports.js')
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+	transpileDependencies: true,
 	css: {
 		loaderOptions: {
-			sass: {
-				additionalData: bootstrapSassAbstractsImports.join('\n')
-			},
 			scss: {
 				additionalData: [...bootstrapSassAbstractsImports, ''].join(';\n')
+			},
+			sass: {
+				additionalData: bootstrapSassAbstractsImports.join('\n')
 			}
 		}
 	}
+})
